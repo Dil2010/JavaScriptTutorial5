8336ba9ca41b9fb2e951c730c4e5eef50b4a9750
Dilan Udawattha <87587934+Dil2010@users.noreply.github.com>
2022-06-19 03:06:18 -0700

    First Commit

---
+++ home.js
@@ -0,0 +1,28 @@
+function ageInDays(){
+var birthyear=prompt("What year you were born in?");
+var ageInDayss=(2018-birthyear)*365;
+
+// var para = document.createElement("p");
+// para.innerText = "This is a paragraph";
+// document.body.appendChild(para);
+
+// var para = document.createElement("p");
+// para.innerText = "This is a paragraph";
+// document.body.appendChild(para);
+
+// var pata=document.createElement("h1");
+// pata.innerText="This is my name";
+// document.body.appendChild(pata);
+
+var para=document.createElement('h1');
+var textAnswer=document.createTextNode('You are '+ageInDayss+' days');
+para.setAttribute('id','ageInDays');
+para.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(para);
+}
+
+
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
